a67ef5aefb37c214bde13bec7eb5317ba51735f2
Scotty <papamoascotty@gmail.com>
2026-07-27 13:27:36 +1200

    Clean cache

--- firmware/ForgeUI-One/main/90_Studio_Export.h
+++ firmware/ForgeUI-One/main/90_Studio_Export.h
@@ -1,15 +1,12 @@
 #pragma once
 
 #include "lvgl.h"
-#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void fg_studio_export_create(lv_obj_t *parent);
-void FG_Set_Status_Light(bool enabled);
-void FG_Set_Status_Status_Indicator(bool enabled);
 
 #ifdef __cplusplus
 }
